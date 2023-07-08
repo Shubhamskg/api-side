b3966f27d1a5b5d83a0ad07b7fd72f7ab97a9677
SHUBHAM KUMAR <shubhamkumargupta786@gmail.com>
2023-07-08 22:12:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(passport.session());
 
 app.use(
   cors({
-    origin: "https://client0.netlify.app/",
+    origin: "https://client0.netlify.app/" || "https://api2-3bx7.onrender.com",
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
   })
