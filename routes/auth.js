f7eee900f9350181d97db0936ade3e48daeb6d29
SHUBHAM KUMAR <shubhamkumargupta786@gmail.com>
2023-07-08 22:39:07 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const passport = require("passport");
 
-const CLIENT_URL = "https://client0.netlify.app";
+const CLIENT_URL = "https://client0.netlify.app/";
 
 router.get("/login/success", (req, res) => {
   if (req.user) {
